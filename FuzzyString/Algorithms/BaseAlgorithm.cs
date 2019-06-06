a7262b2e03dfc753b51037764e9dd70b464ee88a
BV0008 <bruno.vanthournout@externel.be>
2019-06-06 16:05:24 +0200

    add explaining comment

--- FuzzyString/Algorithms/BaseAlgorithm.cs
+++ FuzzyString/Algorithms/BaseAlgorithm.cs
@@ -7,6 +7,10 @@
             return true;
         }
 
+        /// <summary>
+        /// Returns a value between 0.0 and 1.0. 
+        /// The higher the value, the better the match
+        /// </summary>
         public abstract double Calculate(string source, string target);
     }
 }
